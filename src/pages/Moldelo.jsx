130242af0be2be5493ee9d77f039d5b8ea9735b9
LUIZ AUGUSTO <you@example.com>
2025-01-29 21:22:49 -0300

    01

--- src/pages/Moldelo.jsx
+++ src/pages/Moldelo.jsx
@@ -1,12 +1,12 @@
 import moldelo01 from "../img/modelo01.webp";
 import pcs from "../img/pcs.webp";
-import Logo from "../img/Logo.webp";
+import LOGO from "../img/LOGO.png";
 import "../pages/Modelo.css";
 function Modelo(props) {
   return (
     <div>
       <div>
-        <img src={Logo} />
+        <img src={LOGO} />
       </div>
       <header id="nome_produto">{props.PODRUTO}</header>
       <img src={pcs} />
